a655248e61b332bf423b0fff3f17b79add20f0b6
enesozmus <ensozmus@gmail.com>
2021-12-25 01:48:11 +0300

    FirstCommit

---
+++ Koleksiyonlar-Soru-3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static bool IsVowel(char harf)              //  istemler arasında → sesli mi değil mi?
+        {
+            string vowelList = "aeıioöuü";
+            if (vowelList.Contains(harf))
+            {
+                return true;
+            }
+            return false;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Lütfen bir cümle giriniz: ");
+            string sentence = Console.ReadLine();
+            List<char> vowelList = new List<char>();
+
+
+            // Sesliyse Ekle
+            foreach (char vowel in sentence)
+                if (IsVowel(vowel)) vowelList.Add(vowel);
+
+
+            // Sırala
+            char[] vowels = vowelList.ToArray();
+            Array.Sort(vowels);
+
+
+            // Yazdır
+            Console.WriteLine("\nCümlenin İçinde Bulunan Sesli Harfler");
+            foreach (char vowel in vowels)
+                Console.Write(vowel + " ");
+        }
+    }
+}
